544917dce02b1ca8ca3b0a7700a56cda6f5b464b
Jackson Ferguson <jackson.ferguson0@gmail.com>
2026-03-05 13:51:58 -0800

    feat: export and register new environment presets

--- src/protostar/presets/__init__.py
+++ src/protostar/presets/__init__.py
@@ -1,9 +1,12 @@
 """Preset modules for domain-specific environment scaffolding."""
 
+from .api import ApiPreset
 from .astro import AstroPreset
 from .base import PresetModule
+from .cli import CliPreset
 from .dsp import DspPreset
 from .embedded import EmbeddedPreset
+from .ml import MLPreset
 from .scientific import ScientificPreset
 
 PRESETS: tuple[PresetModule, ...] = (
@@ -11,6 +14,9 @@ PRESETS: tuple[PresetModule, ...] = (
     AstroPreset(),
     DspPreset(),
     EmbeddedPreset(),
+    MLPreset(),
+    ApiPreset(),
+    CliPreset(),
 )
 
 __all__ = [
