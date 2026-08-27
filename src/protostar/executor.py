586c8ef37857c8ef66c7a9bbf7c8b8a352ad21a8
Jackson Ferguson <jackson.ferguson0@gmail.com>
2026-08-27 14:04:52 -0700

    feat: migrate to asynchronous static hook registry

--- src/protostar/executor.py
+++ src/protostar/executor.py
@@ -21,6 +21,7 @@ from .manifest import (
     Severity,
     SystemTask,
 )
+from .registry import HookRegistry, RemoteHook
 from .security import enforce_binary_safelist, enforce_path_jail
 from .system import execute_subprocess
 from .toml_ast import merge_toml_payloads
@@ -191,6 +192,8 @@ class SystemExecutor:
         full_yaml = generate_pre_commit_config(
             local_hooks=self.manifest.tooling.pre_commit_local_hooks,
             remote_hooks=self.manifest.tooling.pre_commit_hooks,
+            core_rev=HookRegistry.get_revision(RemoteHook.PRE_COMMIT_HOOKS),
+            gitleaks_rev=HookRegistry.get_revision(RemoteHook.GITLEAKS),
             dependencies=self.manifest.dependencies.dependencies,
             is_prek=self.manifest.tooling.wants_prek,
             install_hook_types=self.manifest.tooling.pre_commit_install_hook_types,
